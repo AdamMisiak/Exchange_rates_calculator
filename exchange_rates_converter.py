c9d3a1097b03f5ccb1b508c8c00a8a80b404fc72
Adam <adammisiak3@gmail.com>
2021-06-30 21:14:03 +0200

    Upgraded script

--- exchange_rates_converter.py
+++ exchange_rates_converter.py
@@ -1,14 +1,28 @@
 import requests
 import datetime
 
-SUNDAY_NUMBER = 6
+FRIDAY_NUMBER = 4
+
+def calculate_multiple_exchange_rates():
+    with open("Input.txt", "r") as file:
+        dates = file.read().splitlines()
+    with open('Output.txt', mode='wt') as result:
+        for date in dates:
+            date_in_datetime_format = datetime.datetime.strptime(date, '%d.%m.%Y')
+            date_prev_day = date_in_datetime_format - datetime.timedelta(days=1)
+            date_converted = date_prev_day.strftime('%Y-%m-%d')
+            # TODO add more currencies
+            result.write('{}\n'.format(calculate_exchange_rates(date_converted, 'USD')))
 
 def calculate_exchange_rates(date, currency):
     if not check_working_day(date):
-        date = get_next_working_day(date)
+        date = get_prev_working_day(date)
 
     url = 'https://api.nbp.pl/api/exchangerates/tables/a/{}'.format(date)
     response = requests.get(url, {'format': 'api'})
+
+    if response.status_code == 404:
+        return 0.000
 
     for rate in response.json()[0]['rates']:
         if rate['code'] == currency:
@@ -16,18 +30,18 @@ def calculate_exchange_rates(date, currency):
     return result
 
 def check_working_day(date_string):
-    date_in_format = datetime.datetime.strptime(date_string, '%Y-%m-%d')
-    return False if date_in_format.weekday() in [5, 6] else True
+    date_in_datetime_format = datetime.datetime.strptime(date_string, '%Y-%m-%d')
+    return False if date_in_datetime_format.weekday() in [5, 6] else True
 
 
-def get_next_working_day(date_string):
-    date_in_format = datetime.datetime.strptime(date_string, '%Y-%m-%d')
+def get_prev_working_day(date_string):
+    date_in_datetime_format = datetime.datetime.strptime(date_string, '%Y-%m-%d')
     # TODO take care of holidays - iterate through next days and send requests - check 404
-    difference = (SUNDAY_NUMBER + 1) - date_in_format.weekday()
-    date_result_in_format = date_in_format + datetime.timedelta(days=difference)
+    difference = date_in_datetime_format.weekday() - FRIDAY_NUMBER 
+    date_result_in_format = date_in_datetime_format - datetime.timedelta(days=difference)
     date_result_string = date_result_in_format.strftime('%Y-%m-%d')
 
     return date_result_string
 
-
-print(calculate_exchange_rates('2021-06-26', 'USD'))
+# print(calculate_exchange_rates('2021-06-26', 'USD'))
+print(calculate_multiple_exchange_rates())
