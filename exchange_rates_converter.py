3b2d41a4be7912719c36c41dde1b332f904f9273
Adam <adammisiak3@gmail.com>
2021-06-30 20:07:23 +0200

    Extended function

--- exchange_rates_converter.py
+++ exchange_rates_converter.py
@@ -1,12 +1,33 @@
 import requests
+import datetime
 
+SUNDAY_NUMBER = 6
 
 def calculate_exchange_rates(date, currency):
-    url = 'https://api.nbp.pl/api/exchangerates/tables/a/{}?format=json'.format(date)
-    response = requests.get(url)
+    if not check_working_day(date):
+        date = get_next_working_day(date)
+
+    url = 'https://api.nbp.pl/api/exchangerates/tables/a/{}'.format(date)
+    response = requests.get(url, {'format': 'api'})
+
     for rate in response.json()[0]['rates']:
         if rate['code'] == currency:
-            print(rate['mid'])
+            result = rate['mid']
+    return result
+
+def check_working_day(date_string):
+    date_in_format = datetime.datetime.strptime(date_string, '%Y-%m-%d')
+    return False if date_in_format.weekday() in [5, 6] else True
 
 
-calculate_exchange_rates('2021-02-05', 'USD')
+def get_next_working_day(date_string):
+    date_in_format = datetime.datetime.strptime(date_string, '%Y-%m-%d')
+    # TODO take care of holidays - iterate through next days and send requests - check 404
+    difference = (SUNDAY_NUMBER + 1) - date_in_format.weekday()
+    date_result_in_format = date_in_format + datetime.timedelta(days=difference)
+    date_result_string = date_result_in_format.strftime('%Y-%m-%d')
+
+    return date_result_string
+
+
+print(calculate_exchange_rates('2021-06-26', 'USD'))
