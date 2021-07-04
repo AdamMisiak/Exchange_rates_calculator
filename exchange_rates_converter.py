4aa20d03a4a86eb42fe643a220d949c8b4420c8c
Adam <adammisiak3@gmail.com>
2021-07-04 14:16:17 +0200

    Upgraded script

--- exchange_rates_converter.py
+++ exchange_rates_converter.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 
 FRIDAY_NUMBER = 4
+URL_BASE = 'https://api.nbp.pl/api/exchangerates/tables/a/'
 
 def calculate_multiple_exchange_rates():
     with open("Input.txt", "r") as file:
@@ -15,33 +16,48 @@ def calculate_multiple_exchange_rates():
             result.write('{}\n'.format(calculate_exchange_rates(date_converted, 'USD')))
 
 def calculate_exchange_rates(date, currency):
-    if not check_working_day(date):
-        date = get_prev_working_day(date)
+    date = get_working_day_date(date)
 
-    url = 'https://api.nbp.pl/api/exchangerates/tables/a/{}'.format(date)
+    url = URL_BASE + date
     response = requests.get(url, {'format': 'api'})
 
     if response.status_code == 404:
-        return 0.000
+        date = get_working_day_before_holiday(date)
 
     for rate in response.json()[0]['rates']:
         if rate['code'] == currency:
             result = rate['mid']
     return result
 
-def check_working_day(date_string):
-    date_in_datetime_format = datetime.datetime.strptime(date_string, '%Y-%m-%d')
-    return False if date_in_datetime_format.weekday() in [5, 6] else True
+def get_working_day_date(date):
+    date_in_datetime_format = datetime.datetime.strptime(date, '%Y-%m-%d')
 
+    if date_in_datetime_format.weekday() in [5, 6]: # 5 and 6 = Saturday and Sunday
+        return get_working_day_before_weekend(date)
+    # else:
+        # url = URL_BASE + date
+        # response = requests.get(url, {'format': 'api'})
+        # if response.status_code == 404:
+        #     return get_working_day_before_holiday(date)
+    return date
 
-def get_prev_working_day(date_string):
-    date_in_datetime_format = datetime.datetime.strptime(date_string, '%Y-%m-%d')
-    # TODO take care of holidays - iterate through next days and send requests - check 404
+def get_working_day_before_weekend(date):
+    date_in_datetime_format = datetime.datetime.strptime(date, '%Y-%m-%d')
     difference = date_in_datetime_format.weekday() - FRIDAY_NUMBER 
-    date_result_in_format = date_in_datetime_format - datetime.timedelta(days=difference)
-    date_result_string = date_result_in_format.strftime('%Y-%m-%d')
-
+    date_result_in_datetime_format = date_in_datetime_format - datetime.timedelta(days=difference)
+    date_result_string = date_result_in_datetime_format.strftime('%Y-%m-%d')
     return date_result_string
 
-# print(calculate_exchange_rates('2021-06-26', 'USD'))
-print(calculate_multiple_exchange_rates())
+def get_working_day_before_holiday(date):
+    date_in_datetime_format = datetime.datetime.strptime(date, '%Y-%m-%d')
+    date_prev_in_datetime_format = date_in_datetime_format
+    url = URL_BASE + date
+    response = requests.get(url, {'format': 'api'})
+    while response.status_code == 404:
+        date_prev_in_datetime_format = date_prev_in_datetime_format - datetime.timedelta(days=1)
+        date_result_string = date_prev_in_datetime_format.strftime('%Y-%m-%d')
+        url = URL_BASE + date_result_string
+        response = requests.get(url, {'format': 'api'})
+    return date_result_string
+
+calculate_multiple_exchange_rates()
