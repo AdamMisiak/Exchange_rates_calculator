8389898f5f218dae1760861d28d06c8720716b09
AdamMisiak <adammisiak3@gmail.com>
2022-06-17 11:05:08 +0200

    Added dates converting

---
+++ dates_converter.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+def convert_dates_format():
+    dates = []
+    with open("Dates.txt", "r") as file:
+        lines = file.read().splitlines()
+        for line in lines:
+            dates.append(datetime.strptime(line, '%d.%m.%Y').strftime("%m/%d/%Y"))
+    print(dates)
+
+
+    with open('Converted_dates.txt', mode='wt') as result:
+        for date in dates:
+            result.write('{}\n'.format(date))
+
+convert_dates_format()
